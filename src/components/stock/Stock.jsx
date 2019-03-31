8eb46d81fcd3a2ac3b018465636f8966641651ca
David Shams <david.shams@gmail.com>
2019-03-31 02:46:40 -0500

    gulp build semantic and place in public

--- src/components/stock/Stock.jsx
+++ src/components/stock/Stock.jsx
@@ -12,12 +12,20 @@ export default class Stock extends Component {
     }
     render() {
         return (
-            <div style={{backgroundColor: 'lightyellow'}}>
+            <div style={{ backgroundColor: 'lightyellow', padding: '25px' }}>
                 <h3>Hightcharts</h3>
                 <button type="button"
                     onClick={this.handleClick}>Launch New Window</button>
+                {/* semantic ui */}
+                <div className="ui labeled button" tabIndex="0">
+                    <div className="ui basic blue button">
+                        <i className="fork icon"></i> Forks</div>
+                    <a className="ui basic left pointing blue label" >
+                        1,048</a>
+                </div>
 
-            </div>
+                {/* semantic ui */}
+            </div >
         )
     }
 }
