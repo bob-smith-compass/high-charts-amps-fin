25f651943d80f635fb6995b5377178dd7e1dd60b
David Shams <david.shams@gmail.com>
2019-03-31 02:12:47 -0500

    open new OpenFin window from React

--- src/components/stock/Stock.jsx
+++ src/components/stock/Stock.jsx
@@ -1,11 +1,22 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 export default class Stock extends Component {
-  render() {
-    return (
-      <div>
-        <h3>Hightcharts</h3>
-      </div>
-    )
-  }
+    constructor(props) {
+        super(props);
+        // this.handleClick = this.handleClick.bind(this)
+    }
+    handleClick() {
+        console.log('From React');
+        window.launchWindow('https://openfin.co/documentation/getting-started/')
+    }
+    render() {
+        return (
+            <div style={{backgroundColor: 'lightyellow'}}>
+                <h3>Hightcharts</h3>
+                <button type="button"
+                    onClick={this.handleClick}>Launch New Window</button>
+
+            </div>
+        )
+    }
 }
